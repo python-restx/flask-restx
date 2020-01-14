c9f90a4b46808d8ee3390b61b665900f169d2d59
ziirish <ziirish@ziirish.info>
2020-01-14 23:24:19 +0100

    allow travis to push releases on PyPi
    This change allows Travis-CI to build and publish new releases on
    PyPi.org upon tag additions.
    The bumpr.rc file has been changed accordingly since we won't use it
    anymore to push new releases directly, but we may still use it to
    prepare new releases (ie. edit the CHANGELOG, README, etc.)

--- flask_restx/__about__.py
+++ flask_restx/__about__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.13.1.dev'
+__version__ = '0.0.1.dev0'
 __description__ = 'Fully featured framework for fast, easy and documented API development with Flask'
