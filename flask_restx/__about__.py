fef171144a6c2c592b3acf44dbd81f71b3c699ec
Yuri Sh <urishmorgun@gmail.com>
2020-03-15 17:09:22 +0200

    Bumped version
    At the moment, the version that's pulled on `pip install` is at least a month older than master

--- flask_restx/__about__.py
+++ flask_restx/__about__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __description__ = (
     "Fully featured framework for fast, easy and documented API development with Flask"
 )
