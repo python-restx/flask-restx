56ee6851039460ad3e19b7b9e48a09ca0d71b7fc
Issa Jubril <issa.jubril@andela.com>
2021-10-02 13:58:52 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ master_doc = "index"
 
 # General information about the project.
 project = u"Flask-RESTX"
-copyright = u"2020, python-restx Authors"
+copyright = u"2021, python-restx Authors"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
