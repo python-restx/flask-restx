45c4a7deab647a49c2ad9ba6edf7f25d583ed1fc
szymonpru <szymon.pruszek@gmail.com>
2021-11-26 20:22:05 +0100

    Fixed handle_error method

--- flask_restx/api.py
+++ flask_restx/api.py
@@ -701,6 +701,8 @@ class Api(object):
         )
         default_data = {}
 
+        result = None
+
         headers = Headers()
 
         for typecheck, handler in six.iteritems(self._own_and_child_error_handlers):
@@ -735,7 +737,7 @@ class Api(object):
         if include_message_in_response:
             default_data["message"] = default_data.get("message", str(e))
 
-        data = getattr(e, "data", default_data)
+        data = getattr(result, "data", default_data)
         fallback_mediatype = None
 
         if code >= HTTPStatus.INTERNAL_SERVER_ERROR:
