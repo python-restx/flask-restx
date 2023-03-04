599592dffc9fec56c8f32e6637aa48c5e9c094d9
Peter Doggart <peter.doggart@pulseai.io>
2023-03-04 12:58:54 +0000

    Bump version 1.1.0

--- flask_restx/__about__.py
+++ flask_restx/__about__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "1.0.7.dev"
+__version__ = "1.1.0"
 __description__ = (
     "Fully featured framework for fast, easy and documented API development with Flask"
 )
