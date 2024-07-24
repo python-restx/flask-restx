c548f0a2d1888c3a286135e69576ca8c5e9ceb78
Peter Doggart <peter.doggart@pulseai.io>
2024-07-24 15:05:00 +0000

    Fix formatting.

--- setup.py
+++ setup.py
@@ -111,5 +111,5 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
     ],
-    python_requires=">=3.8"
+    python_requires=">=3.8",
 )
